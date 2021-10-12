e0c25866385271da2e9dfd7c80100eb97449346a
André Carvalho <afacarvalho@yahoo.com.br>
2021-10-12 09:49:53 -0300

    Some comments of core classes

--- src/model/account.js
+++ src/model/account.js
@@ -1,22 +1,45 @@
 import { ATMConfig } from '../config/atm-config.js';
 
 /**
- * Represents the customer's account and has the balance and transactions
+ * Represents the customer's account and has the balance and transactions.
  */
 class Account {
+    /**
+     * Setting the initial value of an instance.
+     * On fallback starts with ATM configuration values by default.
+     * 
+     * @param {integer} balance, the balance value.
+     * @param {[integer]} transactions, the transactions list.
+     */
     constructor(balance=ATMConfig.account.customerBalance,transactions=[]){
         this.balance=balance;
         this.transactions=transactions;
     };
 
+    /**
+     * Gets the current value of the account balance.
+     * 
+     * @returns {integer} the balance value.
+     */
     getBalance=()=>{
         return this.balance;
     };
 
+    /**
+     * Gets the transactions registered in the account.
+     * 
+     * @returns {[integer]} the transaction list.
+     */
     getTransactions=()=>{
         return this.transactions;
     };
 
+    /**
+     * Take out amount from account.
+     * 
+     * @param {integer} amount, a value to take out from account
+     * @returns {integer} the balance that remains in the account
+     */
     takeOut=(amount)=>{
         if(amount>this.balance) throw Error('This amount exceeds the account balance.');
         this.balance-=amount;
@@ -24,6 +47,10 @@ class Account {
         return this.balance;
     };
 
+    /**
+     * Export data of instance to JSON.
+     * @returns {...} a simple Object
+     */
     simplify=()=>{
         return {
             "balance":this.getBalance(),
--- src/model/cashBox.js
+++ src/model/cashBox.js
@@ -4,21 +4,45 @@ import { ATMConfig } from "../config/atm-config.js";
  * Represents the cash box and has the cash notes available
  */
 class CashBox {
-
+    /**
+     * Setting the initial value of an instance.
+     * On fallback starts with ATM configuration values by default.
+     * 
+     * @param {[{v:integer,a:integer}]} availableNotes, a list of notes and their quantities.
+     */
     constructor(availableNotes=ATMConfig.cashBox.availableNotes){
         this.availableNotes=availableNotes;
     };
     
+    /**
+     * Gets the current value of the available notes list.
+     * 
+     * @returns {[{v:integer,a:integer}]} the list of available notes and their quantities.
+     */
     getAvailableNotes=()=>{
         return this.availableNotes;
     };
 
+    /**
+     * Gets a number of notes for a take out amount and some note value.
+     * 
+     * This method throw an exception if value of note is wrong, zero or negative.
+     * 
+     * @param {integer} amount, an amount to take out.
+     * @param {integer} note, a note value.
+     * @returns the number of notes including ZERO if available notes is not enough.
+     */
     getNumberOfNotes=(amount,note)=>{
         if(note.v<=0) throw Error('Using an incorrect note value. Reset the cash box.');
         let non=parseInt(amount/note.v);// number of notes
         return ((note.a!==Infinity && note.a<non)?(note.a):(non));
     };
     
+    /**
+     * Gets the value total cash available in cash box.
+     * 
+     * @returns {integer} the total cash including ZERO and Infinity
+     */
     getTotalCashAvailable=()=>{
         let availableNotes=this.getAvailableNotes();
         let total=0;
@@ -28,6 +52,15 @@ class CashBox {
         return total;
     };
     
+    /**
+     * Get the number of notes for each note value and an take out amount.
+     * 
+     * This method throws an exception if the total amount of notes in the
+     * cash box is less than the take out amount.
+     * 
+     * @param {integer} amount, an amount to take out.
+     * @returns {[{v:integer,a:integer}]} the list of notes and each note value.
+     */
     getNotesForAmount=(amount)=>{
         let availableNotes=this.getAvailableNotes();
         let outputCash=[];
@@ -44,6 +77,10 @@ class CashBox {
         return outputCash;
     };
 
+    /**
+     * Export data of instance to JSON.
+     * @returns {...} a simple Object
+     */
     simplify=()=>{
         return {
             "availableNotes":this.getAvailableNotes()
